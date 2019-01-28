879ee2cbb986f82e28e3949a6de2bd61f6047a32
olafkarsten <olafkarsten@352745.no-reply.drupal.org>
2019-01-28 06:17:16 +0100

    WIP ...

--- src/StockUpdateInterface.php
+++ src/StockUpdateInterface.php
@@ -19,23 +19,25 @@ interface StockUpdateInterface {
    * @param string $zone
    *   The zone.
    * @param float $quantity
-   *   Tbe quantity.
-   * @param float $unit_cost
-   *   The unit cost.
-   * @param string $currency_code
-   *   The currency code.
+   *   The quantity.
    * @param int $transaction_type_id
    *   The transaction type ID.
+   * @param int $user_id
+   *   Id of the user that created the transaction. In case of an order
+   *   created through the website, this is the same user id as the $order->uid.
+   * @param int|null $order_id
+   *   The order id the transaction belongs to or NULL if the transaction
+   *   was not triggered by an order.
    * @param array $metadata
    *   Holds all the optional values those are:
-   *     - related_tid: related transaction.
-   *     - related_oid: related order.
-   *     - related_uid: related user.
+   *     - related_tid, the related transaction id. (int)
+   *     - unit_cost, the unit cost (float)
+   *     - currency: the currency of the unit cost (string)
    *     - data: Serialized data array.
    *
    * @return int
    *   Return the ID of the transaction.
    */
-  public function createTransaction(PurchasableEntityInterface $entity, $location_id, $zone, $quantity, $unit_cost, $currency_code, $transaction_type_id, array $metadata);
+  public function createTransaction(PurchasableEntityInterface $entity, $location_id, $zone, $quantity, $transaction_type_id, $user_id, $order_id = null, array $metadata);
 
 }
