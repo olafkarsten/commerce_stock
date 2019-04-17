86c2569c885f28d17cfcac847dce75b009cd2295
olafkarsten <olafkarsten@352745.no-reply.drupal.org>
2019-04-17 09:32:16 +0200

    Removed copy/paste error stocktransactionsinterface

--- src/StockTransactionsInterface.php
+++ src/StockTransactionsInterface.php
@@ -15,13 +15,4 @@ interface StockTransactionsInterface {
   const MOVEMENT_FROM = 7;
   const MOVEMENT_TO = 8;
 
-  /**
-   * Gets the transaction type id.
-   *
-   * @return string|int|null
-   *   The transaction type identifier, or NULL if the object does not yet have an
-   *   identifier.
-   */
-  public function id();
-
 }
